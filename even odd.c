1f972c71421c332954ac4428bab051ad437ad679
vishalgaurav28 <67232758+vishalgaurav28@users.noreply.github.com>
2020-07-06 13:58:20 -0700

    Create even odd.c

---
+++ even odd.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include<conio.h>
+
+
+
+void isevenorodd()
+{
+    int x;
+    printf ("enter a number\n");
+    scanf("%d",&x);
+    if
+      (x%2==0)
+      printf("x is even\n",x);
+      else 
+      printf("x is odd\n",x);
+      getch();
+    
+}
+
+void main()
+{
+    isevenorodd();
+}
+
